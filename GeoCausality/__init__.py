dc93157af743225df1a4e437040f17144590a6e0
ConorMcNamara <conor.s.mcnamara@gmail.com>
2025-04-11 14:31:09 -0700

    Added configs to pyproject

--- GeoCausality/__init__.py
+++ GeoCausality/__init__.py
@@ -1,16 +1,19 @@
 """Testing Statistical Hypotheses through Geo Experiments"""
 
-__version__ = "0.2.0"
+__version__ = "0.5.0"
 
 from typing import List
 
-from GeoCausality import diff_in_diff, fixed_effects, geox, synthetic_control
+from GeoCausality import diff_in_diff, fixed_effects, geox, synthetic_control, penalized_synthetic_control, robust_synthetic_control, augmented_synthetic_control
 
 __all__: List[str] = [
     "diff_in_diff",
     "fixed_effects",
     "geox",
     "synthetic_control",
+    "penalized_synthetic_control",
+    "robust_synthetic_control",
+    "augmented_synthetic_control",
 ]
 
 
